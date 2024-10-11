0828ecc521a116479e0bfce9a182f24a2267966f
brandishcode <brandishcode@gmail.com>
2024-10-11 20:11:48 +0900

    Delete unused files

--- src/system/mesh/PrimitiveMesh.cpp
+++
@@ -1,8 +0,0 @@
-#include "PrimitiveMesh.hpp"
-
-namespace Meteora {
-
-PrimitiveMesh::PrimitiveMesh(unsigned int size, Vec3 dimension, Vec3 position)
-    : size(size * sizeof(Vertex)), dimension(dimension), position(position),
-      vertices((Vertex *)malloc(size * sizeof(Vertex))) {}
-} // namespace Meteora
--- src/system/mesh/Square.cpp
+++ src/system/mesh/Square.cpp
@@ -5,24 +5,26 @@ namespace Meteora {
 Square::Square() : Mesh(4, Vec3{1.0f, 1.0f, 0.0f}, 6) {
   float halfLength = dimension.x / 2.0f;
   float halfWidth = dimension.y / 2.0f;
-  vertices.get()[0] =
+  Vertex *ptr = vertices.get();
+  ptr[0] =
       Vertex{Vec3{position.x - halfLength, position.y, position.z - halfWidth},
              Vec3{1.0f, 1.0f, 0.0f}};
-  vertices.get()[1] =
+  ptr[1] =
       Vertex{Vec3{position.x + halfLength, position.y, position.z - halfWidth},
              Vec3{1.0f, 1.0f, 0.0f}};
-  vertices.get()[2] =
+  ptr[2] =
       Vertex{Vec3{position.x - halfLength, position.y, position.z + halfWidth},
              Vec3{1.0f, 1.0f, 0.0f}};
-  vertices.get()[3] =
+  ptr[3] =
       Vertex{Vec3{position.x + halfLength, position.y, position.z + halfWidth},
              Vec3{1.0f, 1.0f, 0.0f}};
 
-  indices.get()[0] = 0;
-  indices.get()[1] = 1;
-  indices.get()[2] = 2;
-  indices.get()[3] = 2;
-  indices.get()[4] = 3;
-  indices.get()[5] = 1;
+  unsigned int *idxPtr = indices.get();
+  idxPtr[0] = 0;
+  idxPtr[1] = 1;
+  idxPtr[2] = 2;
+  idxPtr[3] = 2;
+  idxPtr[4] = 3;
+  idxPtr[5] = 1;
 }
 } // namespace Meteora
